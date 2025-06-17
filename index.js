35ae38c20e802769cc011dd0518580b6b680c867
Victoriaadame <victoriaadame2468@gmail.com>
2025-06-17 20:07:41 +0000

    commit

--- index.js
+++ index.js
@@ -1,12 +1,91 @@
 
 let name = 'mosh';
+let age = 30;
+let isApproved = true;
+let firstName = undefines;
+let selectColor = null;
 console.log(name);
 
 let firstName = 'mosh', lastName = 'hamedani';
 
-let interestRate = 0.3;
+let interestRate = 0.3;git 
 interestRate = 1;
 console.log(interestRate);
 
 
+if(condition) {
+    outcome 1
+} else if (condition) {
+    outcome 2
+} else {
+    outcome 3
+}
 
+&& = AND
+|| = OR 
+!= = NOT EQUAL
+== = IS EQUAL
+
+//Logic: check if they are an adult or not
+if(age >= 18) { // age = 18
+    respond ('You are a legal adult')
+} else if (age < 18) { // age = 16
+    respond 'Youre a minor')
+} else if (age == 20 && sarabellasAge = 20) {
+    respond('Omg, you and sarabella are the same age!')
+} else if (age == 52 && momsAge == 54) {
+    respond('YOURE MOM HAD YOU AT 2 YEARS OLD????')
+} else if (age == 16|| sarabellasAge = 16) {
+    respond('can one of you take me to the store ')
+} else if (age != 15) {
+    respond('You are either too early or too late for a quinceaera!')
+} else { // age = victoria cssi 2025
+    respond('Whatever you responded with does not make sense, lol, read the instructions')
+}
+
+if(name == 'Victoria') {
+    respond('You are authorized to edit this repository')
+} else if (name == 'victoria') {
+    respond('Okay someone didnt read the case sensitive instructions, welcome Victoria')
+} else if (name == 'vICtoria') {
+    respond('Okay someone didnt read the case sensitive instructions, welcome Victoria')
+} else {
+    respond('That input was not accepted, please read the instructions about case sensitivty')
+}
+
+answer = userinput // yes
+//logic: are you majoring in engineering?
+if(answer== 'yes') {
+    respond('I feel sorry for you')
+} else {
+    respond('lucky you')
+]
+
+for(starting variable value; variable restriction; variable behaviour) {
+    code that executes when condition hasnt been met
+}
+
+
+let i = 1; // initialize a variable called 'i' and set it equal to integer 1
+let i = 2'
+let index = 1;
+
+
+i = 2
+i = i + 1 // take the existing value of i, add 1 (increment by 1)
+
+
+i++ IS THE SAME AS i = i + 1
+
+
+// logic: send 5 "Hello Worlds" to console, using a for loop
+for (let i=1; i<=100; i++) {
+    console.log ("Hello World");
+}
+
+    console.log ("Hello World");
+    console.log ("Hello World");
+    console.log ("Hello World");
+    console.log ("Hello World");
+    console.log ("Hello World");
+    console.log ("Hello World");
